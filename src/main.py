f179ae1a3450860b7db810d93668c8455999c55f
Mohammad H. AlShami <mohshami@gmail.com>
2025-03-22 18:07:06 +0000

    Refactor
    Allow setting db_path so healthcheck.py can be run outside of docker
    Refactor checks to allow passing exit codes and messages

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Header, HTTPException
 from fastapi.responses import JSONResponse
-from typing import Optional
+from pydantic_settings import BaseSettings, SettingsConfigDict
 import aiosqlite
 from time import time
 
+
+class Settings(BaseSettings):
+    app_name: str = "healthcheck.py"
+    db_path: str = "/data/db.sqlite"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
 app = FastAPI()
 
 
@@ -11,15 +20,18 @@ app = FastAPI()
 @app.on_event("startup")
 async def startup():
     # Initialize the SQLite connection
-    app.state.db = \
-        await aiosqlite.connect("/data/db.sqlite")
+    app.state.db = await aiosqlite.connect(settings.db_path)
 
-    query = "CREATE TABLE IF NOT EXISTS healthcheck(" + \
-        "id INTEGER PRIMARY KEY AUTOINCREMENT, " + \
-        "name TEXT UNIQUE, " + \
-        "expires LONG," + \
-        "status INTEGER" + \
-        ")"
+    query = (
+        "CREATE TABLE IF NOT EXISTS healthcheck("
+        + "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        + "name TEXT UNIQUE, "
+        + "lastupdate DATETIME DEFAULT CURRENT_TIMESTAMP,"
+        + "grace LONG,"
+        + "output TEXT,"
+        + "exitcode INTEGER"
+        + ")"
+    )
 
     await app.state.db.execute(query)
 
@@ -35,45 +47,81 @@ async def root():
     return {"ready": "yes"}
 
 
-@app.get("/ping/{name}")
-async def ping(name: str,
-               ttl: int | None = Header(default=3600)
-               ):
-    expires = int(time() + ttl)
-    query = 'INSERT INTO healthcheck(name, expires, status) ' + \
-        f'VALUES(\'{name}\', {expires}, 1) ' + \
-        f'ON CONFLICT(name) DO UPDATE SET expires={expires}'
+@app.get("/update/{name}")
+async def update(
+    name: str,
+    grace: int | None = Header(default=3600),
+    output: str | None = Header(default=""),
+    exitcode: int | None = Header(default=0),
+):
+    query = (
+        "INSERT INTO healthcheck(name, grace, output, exitcode) "
+        + f"VALUES('{name}', {grace}, '{output}', {exitcode}) "
+        + f"ON CONFLICT(name) DO UPDATE SET lastupdate=CURRENT_TIMESTAMP, grace={grace}, output='{output}', exitcode={exitcode}"
+    )
 
-    await app.state.db.execute(query)
-    await app.state.db.commit()
+    try:
+        await app.state.db.execute(query)
+        await app.state.db.commit()
+    except Exception:
+        return JSONResponse(status_code=500, content={"status": "Error"})
 
-    return {"expires": expires, "now": int(time())}
-
-
-@app.delete("/ping/{name}")
-async def removeCheck(name: str):
-    query = f'DELETE FROM healthcheck where name="{name}";'
-
-    await app.state.db.execute(query)
-    await app.state.db.commit()
-
-    return {"status": "ok"}
+    return {"status": "Success"}
 
 
 @app.get("/status")
-@app.get("/status/")
-@app.get("/status/{name}")
-async def status(name: Optional[str] = '%'):
-    now = int(time())
-    return_code = 200
-    statustQuery = f"select name, expires from healthcheck " \
-        f"where name like '{name}'"
-    cursor = await app.state.db.execute(statustQuery)
-    records = await cursor.fetchall()
-    checks = {}
-    for (key, val) in records:
-        checks[key] = val - now
-        if val < now:
-            return_code = 503
+async def statusall():
+    query = (
+        "select count(id) from healthcheck where "
+        + "unixepoch(lastupdate)+grace < unixepoch(CURRENT_TIMESTAMP) "
+        + " or exitcode != 0"
+    )
 
-    return JSONResponse(status_code=return_code, content=checks)
+    try:
+        cursor = await app.state.db.execute(query)
+        record = await cursor.fetchone()
+    except Exception:
+        return JSONResponse(status_code=500, content={"status": "Error"})
+
+    if int(record[0]) == 0:
+        return {"status": "OK"}
+
+    return {"status": "Error", "Message": "At least one check failed or expired"}
+
+
+@app.get("/status/{name}")
+async def status(name: str):
+    query = f"select unixepoch(lastupdate), grace, output, exitcode from healthcheck where name='{name}'"
+
+    try:
+        cursor = await app.state.db.execute(query)
+        record = await cursor.fetchone()
+    except Exception:
+        return JSONResponse(status_code=500, content={"status": "Error"})
+
+    if record is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Check not found",
+        )
+
+    return {
+        "lastupdate": record[0],
+        "grace": record[1],
+        "output": "" if record[2] is None else record[2],
+        "status": record[3],
+        "expired": (record[0] + record[1]) < time(),
+    }
+
+
+@app.get("/delete/{name}")
+async def removeCheck(name: str):
+    query = f'DELETE FROM healthcheck where name="{name}";'
+
+    try:
+        await app.state.db.execute(query)
+        await app.state.db.commit()
+    except Exception:
+        return JSONResponse(status_code=500, content={"status": "Error"})
+
+    return {"status": "ok"}
